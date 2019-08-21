8fc60952d387a9b01caf3c71d31f74a6ac2fcae0
Rameez Khan <rameezkhan.sa@gmail.com>
2019-08-21 08:34:24 +0200

    .

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -4,4 +4,19 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "I'm a little teapot!"))
+
+(defn train
+  []
+  (println "Choo choo!"))
+
+(defn rameez
+  []
+  (println "...is the best..."))
+
+(def name
+  "Rameez")
+
+(defn greet
+  []
+  (println (str "Hi there " name)))
