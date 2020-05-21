c98ce6fc78c14d4e7f8f14a6abf37a1fcb4a1f92
Rameez Khan <rameezkhan.sa@gmail.com>
2020-05-21 09:55:22 +0200

    Add more practice examples

--- brave-clojure/part-1/clojure-noob/src/clojure_noob/core.clj
+++ brave-clojure/part-1/clojure-noob/src/clojure_noob/core.clj
@@ -10,3 +10,35 @@
 (defn train
   []
   (println "Choo choo!"))
+
+(defn my-thing
+  [[first-thing]]
+  first-thing)
+
+(defn chooser
+  [[first-choice second-choice & other-choices]]
+  (println "First choice is " first-choice)
+  (println "Second choice is " second-choice)
+  (println "Don't care about the other choices")
+  (println (str "But here they are just in case you wanna cry about them" (clojure.string/join ", " other-choices))))
+
+(defn announce-treasure-location
+  [{:keys [lat lng] :as treasure-location}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lng: " lng))
+  treasure-location)
+
+(def power2 (fn [x] (* x x)))
+
+(#(* % %) 8)
+
+(map #(str "Hi there " % "!") ["Rameez" "Bob"])
+
+(defn inc-maker
+  "Create a custom inc maker"
+  [inc-by]
+  #(+ % inc-by))
+
+(def inc4 (inc-maker 4))
+
+(inc4 10)
